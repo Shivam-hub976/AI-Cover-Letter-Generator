7b12504a2773d60d04fca464dea59f9d3ef9b73b
Shivam-hub976 <kumarshivambgs407@gmail.com>
2026-07-12 11:29:42 +0530

    implemented PDF parsing and dynamic resume personalization in LLM payload

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const multer = require('multer'); //handles file uploads
+const pdfParse = require('pdf-parse'); // Reads pdf text
 const { GoogleGenerativeAI } = require('@google/generative-ai');
 
 // Set up the Express server
@@ -8,14 +10,25 @@ const app = express();
 app.use(cors()); //Allows HTML frontend to communicate with this backend
 app.use(express.json()); // Allows the server to read the incoming JSON data
 
+// Configure multer to store the uploaded file temporarily in memory (RAM)
+const upload = multer({storage: multer.memoryStorage()});
+
+
 // Initialize the Gemini using the secure key from the .env file
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 
 // Create the API endpoint that frontend will call
-app.post('/generate', async(req, res) => {
+app.post('/generate', upload.single('resume'), async(req, res) => {
     try{
         //Capture the data sent from the frontend form
         const { name, role, company, skills} = req.body;
+
+        // Parse the PDF file if it exists
+        let resumeText = "";
+        if (req.file){
+            const pdfData = await pdfParse(req.file.buffer);
+            resumeText = pdfData.text;
+        }
 
         //Programmatic prompt engineering
         const prompt = `You are an expert career coach. Write a professional, concise, and compelling cover letter for the following candidate:
@@ -24,12 +37,17 @@ app.post('/generate', async(req, res) => {
         - Target Company: ${company}
         - Key Skills: ${skills}
         
+        Here is the candidate's actual resume text. Use this to highly contextualize the letter and highlight relevant achievements. 
+        RESUME TEXT: 
+        ${resumeText}
+        
         STRICT FORMATTING RULES:
-        1. Start the letter immediately with the greeting (e.g., "Dear ${company} Hiring Team,").
-        2. ABSOLUTELY NO HEADERS. Do not include placeholders for Date, Email, Phone Number, or Address.
-        3. DO NOT use brackets like [ ] anywhere in the response.
-        4. Do not hallucinate experiences outside of the provided skills.
-        5. End exactly with: 
+        1. Start immediately with the greeting (e.g., "Dear ${company} Hiring Team,").
+        2. ABSOLUTELY NO HEADERS (No Date, Email, Phone, Address).
+        3. DO NOT use brackets like [ ].
+        4. Do not hallucinate experiences outside of the provided skills or resume.
+        5. Use standard paragraph breaks.
+        6. End exactly with: 
         Sincerely,
         ${name}
         
